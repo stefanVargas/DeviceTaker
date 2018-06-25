2c2b0ef60618752dda006cadbf1036af5ef94bb6
stefanv.demoraes <stefanvargas3@gmail.com>
2018-06-25 17:39:21 -0300

    Initial Commit

---
+++ DeviceTaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceTaker
+//
+//  Created by Stefan V. de Moraes on 25/06/2018.
+//  Copyright © 2018 Stefan V. de Moraes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
